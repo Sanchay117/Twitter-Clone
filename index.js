bef276f5ecefc41dedaaec2e8cf98b16d89fd1ef
Sanchay Singh <sanchay072@gmail.com>
2025-03-20 19:12:41 +0530

    WIP

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const toggleBtn = document.getElementsByClassName('theme')[0];
-toggleBtn.addEventListener('click', () => {
+const themeToggle = document.querySelector('.theme');
+
+themeToggle.addEventListener('click', () => {
     const currentTheme = document.documentElement.getAttribute('data-bs-theme');
     document.documentElement.setAttribute('data-bs-theme', currentTheme === 'light' ? 'dark' : 'light');
+    if (currentTheme === "light"){
+        themeToggle.innerHTML = '<i class="fa-solid fa-moon moon-icon"></i>';
+    }else themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
 });
+
