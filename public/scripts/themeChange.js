019f7fcae227555b45bd2a77616a3ab4fecc37b2
Sanchay Singh <sanchay072@gmail.com>
2025-04-09 17:12:37 +0530

    wip

--- public/scripts/themeChange.js
+++ public/scripts/themeChange.js
@@ -9,12 +9,4 @@ themeToggle.addEventListener('click', () => {
     if (currentTheme === 'light') {
         themeToggle.innerHTML = '<i class="fa-solid fa-moon moon-icon"></i>';
     } else themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
-    const root = document.documentElement;
-
-    const darkBlack = getComputedStyle(root).getPropertyValue('--dark-black');
-    const darkWhite = getComputedStyle(root).getPropertyValue('--dark-white');
-
-    // Swap them
-    root.style.setProperty('--dark-black', darkWhite.trim());
-    root.style.setProperty('--dark-white', darkBlack.trim());
 });
